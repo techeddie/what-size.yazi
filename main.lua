a1ecd647ada7b851a0fff5752942cb1585d7761a
Eddie <eddie@directbox.de>
2025-06-19 23:45:56 +0200

    original

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+
 -- function to get paths of selected elements or current directory
 -- if no elements are selected
 local get_paths = ya.sync(function()
@@ -20,15 +21,15 @@ end)
 -- Function to get total size from output
 -- Unix use `du`, Windows use PowerShell
 local function get_total_size(items)
-  local is_windows = package.config:sub(1, 1) == "\\"
+  local is_windows = package.config:sub(1,1) == '\\'
 
   if is_windows then
     local total = 0
     for _, path in ipairs(items) do
       path = path:gsub('"', '\\"')
       local ps_cmd = string.format(
-        [[powershell -Command "& { $p = '%s'; if (Test-Path $p) { if ((Get-ChildItem -Path $p -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object Length -Sum).Sum) { (Get-ChildItem -Path $p -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object Length -Sum).Sum } else { (Get-Item $p).Length } } }"]],
-        path
+      [[powershell -Command "& { $p = '%s'; if (Test-Path $p) { if ((Get-ChildItem -Path $p -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object Length -Sum).Sum) { (Get-ChildItem -Path $p -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object Length -Sum).Sum } else { (Get-Item $p).Length } } }"]],
+      path
       )
       local pipe = io.popen(ps_cmd)
       local result = pipe:read("*a")
@@ -40,9 +41,7 @@ local function get_total_size(items)
       -- }
       pipe:close()
       local num = tonumber(result)
-      if num then
-        total = total + num
-      end
+      if num then total = total + num end
     end
     return total
   else
@@ -79,21 +78,25 @@ local function format_size(size)
 end
 
 return {
-  entry = ya.sync(function(_, job)
+  -- as per doc: https://yazi-rs.github.io/docs/plugins/overview#functional-plugin
+  entry = function(_, job)
+    -- defaults not to use clipboard, use it only if required by the user
     local clipboard = job.args.clipboard == true or job.args[1] == "--clipboard" or job.args[1] == "-c"
     local items = get_paths()
 
     local total_size = get_total_size(items)
     local formatted_size = format_size(total_size)
 
+    local notification_content = "Total size: " .. formatted_size
     if clipboard then
       ya.clipboard(formatted_size)
+      notification_content = notification_content .. "\nCopied to clipboard."
     end
 
-    ya.notify({
+    ya.notify {
       title = "What size",
       content = notification_content,
       timeout = 4,
-    })
-  end),
+    }
+  end,
 }
